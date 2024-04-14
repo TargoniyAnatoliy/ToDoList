64f3d8150aaecbd0598aef1e85de0fea767bd0a9
Anatoliy <targoniyanatoliy@gmail.com>
2024-04-14 21:31:36 +0300

    Update script.js
    add function Task with prototypes

--- script.js
+++ script.js
@@ -1,69 +1,9 @@
 const listWrapper = document.querySelector('.list-wrapper');
-const listHeader = document.querySelector('#list-header');
-let listInput = document.querySelector('#list-input');
-const addBtn = document.querySelector('#add-button');
-let list = document.querySelector('#list');
-let startMsg = document.querySelector('#start-message');
 let tasks = [];
-
-addBtn.addEventListener('click', addNewElementHandler);
-//додавання елемента при натисканні "Enter"
-listInput.addEventListener('keydown', (e) => {
-    if(e.code === 'Enter') {
-        addNewElementHandler();
-    }
-});
-
-//створюємо новий елемент у списку
-function addNewElementHandler() {
-    let inputValue = listInput.value;
-    tasks.push(inputValue);
-    let li = document.createElement('li');
-    li.classList.add('list-item');
-    let liText = document.createTextNode(inputValue);
-
-    if(!startMsg.hidden) {
-        startMsg.hidden = true;
-    }
-
-    if(inputValue === '') { 
-        alert('Can you write something?');
-        return;
-     } else { 
-        li.appendChild(liText);
-    }
-
-    list.appendChild(li);
-    listInput.value = '';
-
-    let liCloseBtn = createLiCloseBtn();
-    li.appendChild(liCloseBtn);
-}
-
-
-function createLiCloseBtn() {
-    let span = document.createElement('span');
-    span.className = 'close';
-    let spanText = document.createTextNode('\u00D7');
-    span.appendChild(spanText);
-    return span;
-}
-
-list.addEventListener('click', removeOrSelectHandler);
-
-//додавання класу "checked" при кліку на li або видалення завдання при кліку на "X"
-function removeOrSelectHandler(e) {
-    switch(e.target.className) {
-        case 'close': e.target.parentElement.remove();
-                    //якщо немає завдань, то показуємо стартове повідомлення
-                    if((startMsg.hidden) && (list.children.length <= 1)) {
-                        startMsg.hidden = false;
-                    }
-            break;
-        case 'list-item': e.target.classList.toggle('checked');
-            break;
-    }    
-}
+let input = document.querySelector('#list-input');
+const addBtn = document.querySelector('#add-button');
+let startMsg = document.querySelector('#start-message');
+let tasksList = document.querySelector('#list');
 
 //переміщення списку по сторінці
 let move = false;
@@ -88,3 +28,60 @@ document.addEventListener('mousemove', (e) => {
 document.addEventListener('mouseup', (e) => {
     move = false;
 });
+
+addBtn.addEventListener('click', addTaskHandler);
+
+input.addEventListener('keydown', function (e) {
+    if(e.code === 'Enter') addTaskHandler();
+});
+
+function addTaskHandler() {
+    if(input.value) {
+        if(!startMsg.hidden) startMsg.hidden = true;        
+
+        let newTask = new Task(input.value);
+        newTask.createTaskElement(tasksList);
+        newTask.createLiCloseBtn(tasksList);
+
+        tasks.push(newTask);
+
+        input.value = '';
+    } else {
+        alert(`Add task name!`);
+    }
+}
+
+function Task(inputText) {
+    this.text = inputText;
+    this.checked = false;
+    this.li = null;
+    this.span = null;
+}
+
+Task.prototype.createTaskElement = function (list) {
+    this.li = document.createElement('li');
+    this.li.classList.add('list-item');
+    this.li.textContent = this.text;
+    list.append(this.li);
+}
+
+Task.prototype.createLiCloseBtn = function (list) {
+        this.span = document.createElement('span');
+        this.span.classList.add('close');
+        this.span.textContent = '\u00D7';
+        this.li.append(this.span);
+}
+
+tasksList.addEventListener('click', removeOrSelectHandler);
+
+//додавання класу "checked" при кліку на li або видалення завдання при кліку на "X"
+function removeOrSelectHandler(e) {
+    if(e.target.className === 'close') {
+        e.target.parentElement.remove();
+        //якщо немає завдань, то показуємо стартове повідомлення
+        if((startMsg.hidden) && (list.children.length <= 1)) {
+            startMsg.hidden = false;
+        }
+    }
+    if(e.target.classList.contains('list-item')) e.target.classList.toggle('checked');   
+}
